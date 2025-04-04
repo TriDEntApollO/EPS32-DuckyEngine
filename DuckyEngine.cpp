51bdc710bcbd2f8d2fc45950aa360045918bab64
Trident Apollo <tridentapollo.llgic@gmail.com>
2025-04-04 22:43:09 +0530

    Added Full key support

--- DuckyEngine.cpp
+++ DuckyEngine.cpp
@@ -4,20 +4,85 @@
 
 DuckyEngine::DuckyEngine(BleKeyboard &bleKeyboard) : bleKeyboard(bleKeyboard), intervalDelay(5) {
     keyMap = {
+        // Basic keys
         {"ENTER", KEY_RETURN},
-        {"CTRL", KEY_LEFT_CTRL},
-        {"ALT", KEY_LEFT_ALT},
-        {"SHIFT", KEY_LEFT_SHIFT},
-        {"GUI", KEY_LEFT_GUI},
         {"ESC", KEY_ESC},
         {"TAB", KEY_TAB},
         {"SPACE", ' '},
         {"BACKSPACE", KEY_BACKSPACE},
         {"DELETE", KEY_DELETE},
+
+        // Modifier keys
+        {"CTRL", KEY_LEFT_CTRL},
+        {"SHIFT", KEY_LEFT_SHIFT},
+        {"ALT", KEY_LEFT_ALT},
+        {"GUI", KEY_LEFT_GUI},
+        {"LEFT_CTRL", KEY_LEFT_CTRL},
+        {"LEFT_SHIFT", KEY_LEFT_SHIFT},
+        {"LEFT_ALT", KEY_LEFT_ALT},
+        {"LEFT_GUI", KEY_LEFT_GUI},
+        {"RIGHT_CTRL", KEY_RIGHT_CTRL},
+        {"RIGHT_SHIFT", KEY_RIGHT_SHIFT},
+        {"RIGHT_ALT", KEY_RIGHT_ALT},
+        {"RIGHT_GUI", KEY_RIGHT_GUI},
+
+        // Navigation keys
         {"UP", KEY_UP_ARROW},
         {"DOWN", KEY_DOWN_ARROW},
         {"LEFT", KEY_LEFT_ARROW},
         {"RIGHT", KEY_RIGHT_ARROW},
+        {"INSERT", KEY_INSERT},
+        {"HOME", KEY_HOME},
+        {"END", KEY_END},
+        {"PAGE_UP", KEY_PAGE_UP},
+        {"PAGE_DOWN", KEY_PAGE_DOWN},
+
+        // Function keys
+        {"F1", KEY_F1}, {"F2", KEY_F2}, {"F3", KEY_F3}, {"F4", KEY_F4},
+        {"F5", KEY_F5}, {"F6", KEY_F6}, {"F7", KEY_F7}, {"F8", KEY_F8},
+        {"F9", KEY_F9}, {"F10", KEY_F10}, {"F11", KEY_F11}, {"F12", KEY_F12},
+        {"F13", KEY_F13}, {"F14", KEY_F14}, {"F15", KEY_F15}, {"F16", KEY_F16},
+        {"F17", KEY_F17}, {"F18", KEY_F18}, {"F19", KEY_F19}, {"F20", KEY_F20},
+        {"F21", KEY_F21}, {"F22", KEY_F22}, {"F23", KEY_F23}, {"F24", KEY_F24},
+
+        // Alphanumeric cluster
+        {"CAPS_LOCK", KEY_CAPS_LOCK},
+        {"MENU", KEY_MENU},
+
+        // Function control cluster
+        {"PRINT_SCREEN", KEY_PRINT_SCREEN},
+        {"SCROLL_LOCK", KEY_SCROLL_LOCK},
+        {"PAUSE", KEY_PAUSE},
+
+        // Keypad keys
+        {"NUM_LOCK", KEY_NUM_LOCK},
+        {"KP_SLASH", KEY_KP_SLASH},
+        {"KP_ASTERISK", KEY_KP_ASTERISK},
+        {"KP_MINUS", KEY_KP_MINUS},
+        {"KP_PLUS", KEY_KP_PLUS},
+        {"KP_ENTER", KEY_KP_ENTER},
+        {"KP_1", KEY_KP_1}, {"KP_2", KEY_KP_2}, {"KP_3", KEY_KP_3},
+        {"KP_4", KEY_KP_4}, {"KP_5", KEY_KP_5}, {"KP_6", KEY_KP_6},
+        {"KP_7", KEY_KP_7}, {"KP_8", KEY_KP_8}, {"KP_9", KEY_KP_9},
+        {"KP_0", KEY_KP_0}, {"KP_DOT", KEY_KP_DOT},
+
+        // Media keys
+        {"MEDIA_NEXT_TRACK", KEY_MEDIA_NEXT_TRACK},
+        {"MEDIA_PREVIOUS_TRACK", KEY_MEDIA_PREVIOUS_TRACK},
+        {"MEDIA_STOP", KEY_MEDIA_STOP},
+        {"MEDIA_PLAY_PAUSE", KEY_MEDIA_PLAY_PAUSE},
+        {"MEDIA_MUTE", KEY_MEDIA_MUTE},
+        {"MEDIA_VOLUME_UP", KEY_MEDIA_VOLUME_UP},
+        {"MEDIA_VOLUME_DOWN", KEY_MEDIA_VOLUME_DOWN},
+        {"MEDIA_WWW_HOME", KEY_MEDIA_WWW_HOME},
+        {"MEDIA_LOCAL_MACHINE_BROWSER", KEY_MEDIA_LOCAL_MACHINE_BROWSER},
+        {"MEDIA_CALCULATOR", KEY_MEDIA_CALCULATOR},
+        {"MEDIA_WWW_BOOKMARKS", KEY_MEDIA_WWW_BOOKMARKS},
+        {"MEDIA_WWW_SEARCH", KEY_MEDIA_WWW_SEARCH},
+        {"MEDIA_WWW_STOP", KEY_MEDIA_WWW_STOP},
+        {"MEDIA_WWW_BACK", KEY_MEDIA_WWW_BACK},
+        {"MEDIA_CONSUMER_CONTROL_CONFIGURATION", KEY_MEDIA_CONSUMER_CONTROL_CONFIGURATION},
+        {"MEDIA_EMAIL_READER", KEY_MEDIA_EMAIL_READER}
     };
 }
 
@@ -224,7 +289,12 @@ void DuckyEngine::delayExecution(int milliseconds) {
 
 // Checks if a key is a modifier
 bool DuckyEngine::isModifierKey(uint8_t keyCode) {
-    return (keyCode == KEY_LEFT_CTRL || keyCode == KEY_LEFT_ALT || keyCode == KEY_LEFT_SHIFT || keyCode == KEY_LEFT_GUI);
+    return (
+        keyCode == KEY_LEFT_CTRL  || keyCode == KEY_RIGHT_CTRL  ||
+        keyCode == KEY_LEFT_SHIFT || keyCode == KEY_RIGHT_SHIFT ||
+        keyCode == KEY_LEFT_ALT   || keyCode == KEY_RIGHT_ALT   ||
+        keyCode == KEY_LEFT_GUI   || keyCode == KEY_RIGHT_GUI
+    );
 }
 
 // Removes a modifier key from activeModifiers
